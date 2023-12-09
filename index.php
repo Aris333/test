a3041d9931e2d23d8ad1f05dee4ba4e0199a69e2
Aris333 <hamzaalibhatti33@gmail.com>
2023-12-09 18:54:17 +0000

    outer files

---
+++ index.php
@@ -0,0 +1,22 @@
+
+  
+
+<!DOCTYPE html>
+<html>
+    <head>
+       <Title>
+           SpeedyFax App
+       </Title> 
+       <link rel="icon" type="image/x-icon" href="https://speedyfaxapp.com/api//assests/images/logo_transparent.png">
+    
+<body>
+<h1 style="color: blueviolet;text-align: center;">Download the App from App Store : SpeedyFax App </h1>
+  <h2> </h2>
+<video controls autoplay loop style="
+    width: -webkit-fill-available;">
+  <source src="video.mp4" type="video/mp4">
+</video>
+
+</body>
+</head>
+</html>
